dd6ee3df08e9bfe06dc4aa6b74250fe7507b0bc5
BernardoTobon <bernardotobon1@gmail.com>
2023-05-08 18:00:02 -0500

    create for example

---
+++ constants/index.js
@@ -0,0 +1,31 @@
+export const userDataDetail = [
+    {
+      name2: "julio",
+      lastName: "jaramillo",
+      userId: "12321312",
+      email: "jarami@gmail.com",
+    },
+    {
+      name2: "dario",
+      lastName: "gomez",
+      userId: "3434534534",
+      email: "dario@gmail.com",
+    },
+    {
+      name2: "diomedez",
+      lastName: "diaz",
+      userId: "789789789",
+      email: "dias@gmail.com",
+    }
+]
+
+export const userData = {
+    
+      name2: "diomedez",
+      lastName: "diaz",
+      userId: "789789789",
+      email: "dias@gmail.com",
+      profile:{
+        name:"",
+      }
+}
