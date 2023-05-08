12093a5b4f808d924b2a53749f946d004eedefb7
BernardoTobon <bernardotobon1@gmail.com>
2023-05-08 18:00:44 -0500

    Add card

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,38 @@
 import Link from "next/link";
 import Accordion from "./Accordion";
 import Calendar from "./DatePicker";
-import Tilt from 'react-parallax-tilt'
+import Tilt from "react-parallax-tilt";
+import UserCard from "./UserCard";
+import { userDataDetail } from "../../constants";
 
 const StartHome = () => {
+  
   return (
     <>
       <div className="w-full h-screen  bg-tea-green overflow-auto">
-        
         <div className="flex justify-center">
-        <Tilt>
-          <img className="h-70 w-[70vw] md:w-96 md:h-80 lg:w-96 lg:h-80 border-2 m-6 border-dark-green " src="/apnl.png" />
+          <Tilt>
+            <img
+              className="h-70 w-[70vw] md:w-96 md:h-80 lg:w-96 lg:h-80 border-2 m-6 border-dark-green "
+              src="/apnl.png"
+            />
           </Tilt>
         </div>
-        
-        
+
         <div className="flex justify-center">
-          <div className="w-[60vw]">
-          <Accordion>
-            <Calendar />
-          </Accordion>
+          <div className="w-[80vw] bg-white md:w-[60vw]">
+            <Accordion>
+              <Calendar />
+            </Accordion>
           </div>
+        </div>
+
+        <div className="flex justify-center flex-col items-center md:flex-row">
+          {
+            userDataDetail.map(
+              (user,index) => <UserCard key={index} {...user} name={user.name2}/>
+            )
+          }
         </div>
       </div>
     </>
