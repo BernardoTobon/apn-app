284e3822c7eb85d057f4f0dffe133955f1d25bc6
BernardoTobon <bernardotobon1@gmail.com>
2023-05-09 16:29:41 -0500

    change for test it

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,24 +1,25 @@
-const UserCard = ({name, lastName, userId, email, imgUrl}) => {
+const UserCard = ({ name, imgUrl }) => {
   return (
-    <div className="flex flex-col w-[20vw] h=[20vw]">
-      <img
-        className="m-2 rounded-full"
-        src={imgUrl || "https://i.pinimg.com/236x/e9/57/2a/e9572a70726980ed5445c02e1058760b.jpg"}
-      />
-      <div className="flex justify-center m-1">
-        <input value={name} className="border border-2 w-[35vw]" placeholder="Nombre" />
+    <div className="flex flex-row">
+      <div>
+        <img
+          className="rounded-full h-24 w-24 "
+          src={
+            imgUrl ||
+            "https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-Pic.png"
+          }
+        />
       </div>
-      <div className="flex justify-center m-1">
-        <input value={lastName} className="border border-2 w-[35vw]" placeholder="Apellido" />
-      </div>
-      <div className="flex justify-center m-1">
-        <input value={userId} className="border border-2 w-[35vw]" placeholder="Cedulda" />
-      </div>
-      <div className="flex justify-center m-1">
-        <input value={email} className="border border-2 w-[35vw]" placeholder="E-mail" />
+      <div className="flex justify-center">
+        <input
+          value={name}
+          disabled
+          className="border border-2 border-dark-green w-40 h-10 m-2 px-8 rounded-lg"
+          placeholder="Nombre"
+        />
       </div>
     </div>
   );
 };
 
-export default UserCard
+export default UserCard;
