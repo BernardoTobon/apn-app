a332e31502a298adbc3180436324ff41935be645
BernardoTobon <bernardotobon1@gmail.com>
2023-05-09 20:07:19 -0500

    add styles on label

--- src/components/Data.js
+++ src/components/Data.js
@@ -14,7 +14,7 @@ const UserData = () => {
           <form>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -29,7 +29,7 @@ const UserData = () => {
                       d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
                     />
                   </svg>
-                  Nombre
+                  <span className="text-light-brown">Nombre</span> 
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
@@ -37,7 +37,7 @@ const UserData = () => {
                 />
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -52,7 +52,7 @@ const UserData = () => {
                       d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
                     />
                   </svg>
-                  Primer Apellido
+                  <span className="text-light-brown">Primer Apellido </span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
@@ -60,7 +60,7 @@ const UserData = () => {
                 />
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -75,7 +75,7 @@ const UserData = () => {
                       d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
                     />
                   </svg>
-                  Segundo Apellido
+                  <span className="text-light-brown">Segundo Apellido</span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
@@ -83,7 +83,7 @@ const UserData = () => {
                 />
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -98,7 +98,7 @@ const UserData = () => {
                       d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
                     />
                   </svg>
-                  DI
+                  <span className="text-light-brown">DI</span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
@@ -106,7 +106,7 @@ const UserData = () => {
                 />
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -121,7 +121,7 @@ const UserData = () => {
                       d="M12 8.25v-1.5m0 1.5c-1.355 0-2.697.056-4.024.166C6.845 8.51 6 9.473 6 10.608v2.513m6-4.87c1.355 0 2.697.055 4.024.165C17.155 8.51 18 9.473 18 10.608v2.513m-3-4.87v-1.5m-6 1.5v-1.5m12 9.75l-1.5.75a3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0L3 16.5m15-3.38a48.474 48.474 0 00-6-.37c-2.032 0-4.034.125-6 .37m12 0c.39.049.777.102 1.163.16 1.07.16 1.837 1.094 1.837 2.175v5.17c0 .62-.504 1.124-1.125 1.124H4.125A1.125 1.125 0 013 20.625v-5.17c0-1.08.768-2.014 1.837-2.174A47.78 47.78 0 016 13.12M12.265 3.11a.375.375 0 11-.53 0L12 2.845l.265.265zm-3 0a.375.375 0 11-.53 0L9 2.845l.265.265zm6 0a.375.375 0 11-.53 0L15 2.845l.265.265z"
                     />
                   </svg>
-                  Fecha De Nacimiento
+                  <span className="text-light-brown">Fecha De Nacimiento</span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
@@ -129,7 +129,7 @@ const UserData = () => {
                 />
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -144,7 +144,7 @@ const UserData = () => {
                       d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
                     />
                   </svg>
-                  Edad
+                  <span className="text-light-brown">Edad</span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
@@ -152,7 +152,7 @@ const UserData = () => {
                 />
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -167,7 +167,7 @@ const UserData = () => {
                       d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
                     />
                   </svg>
-                  Genero
+                  <span className="text-light-brown">Genero</span>
                 </label>
                 <select className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg">
                   <option>Hombre</option>
@@ -175,7 +175,7 @@ const UserData = () => {
                 </select>
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -190,7 +190,7 @@ const UserData = () => {
                       d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
                     />
                   </svg>
-                  Escolaridad
+                  <span className="text-light-brown">Escolaridad</span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
@@ -201,7 +201,7 @@ const UserData = () => {
             <h1 className="m-3 flex justify-center font-extrabold  text-transparent text-2xl bg-clip-text bg-gradient-to-r from-light-brown to-dark-green">Datos De Contacto</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -216,7 +216,7 @@ const UserData = () => {
                       d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
                     />
                   </svg>
-                  Celular
+                  <span className="text-light-brown">Celuklar</span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
@@ -224,7 +224,7 @@ const UserData = () => {
                 />
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -239,7 +239,7 @@ const UserData = () => {
                       d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
                     />
                   </svg>
-                  Telefono Fijo
+                  <span className="text-light-brown">Telefono Fijo</span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
@@ -247,7 +247,7 @@ const UserData = () => {
                 />
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -261,7 +261,7 @@ const UserData = () => {
                       d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
                     />
                   </svg>
-                  E-mail
+                  <span className="text-light-brown">E-mail</span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
@@ -269,7 +269,7 @@ const UserData = () => {
                 />
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -284,7 +284,7 @@ const UserData = () => {
                       d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25"
                     />
                   </svg>
-                  Direccion
+                  <span className="text-light-brown">Direccion</span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
@@ -292,7 +292,7 @@ const UserData = () => {
                 />
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -307,15 +307,15 @@ const UserData = () => {
                       d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
                     />
                   </svg>
-                  Ciudad
+                  <span className="text-light-brown">Ciudad</span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
-                  type="date"
+                  type="text"
                 />
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
-                <label className="flex justify-center">
+                <label className="flex justify-center text-dark-green">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -330,7 +330,7 @@ const UserData = () => {
                       d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
                     />
                   </svg>
-                  Departamento
+                  <span className="text-light-brown">Departamento</span>
                 </label>
                 <input
                   className="text-center border border-2 border-dark-green focus:outline-none m-1 rounded-lg"
