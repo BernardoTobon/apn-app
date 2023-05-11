54cc1079d3fce12f5a8d2e57eb33794ab35f154b
BernardoTobon <bernardotobon1@gmail.com>
2023-05-11 11:24:15 -0500

    import constants and add it to code

--- src/components/LifeStyle.js
+++ src/components/LifeStyle.js
@@ -1,6 +1,23 @@
-import { Info, Schedule, Sports, Stress, Work, WorkDescription } from "@/icons";
+import {
+  Frequiency,
+  Info,
+  Schedule,
+  Sports,
+  Stress,
+  Work,
+  WorkDescription,
+} from "@/icons";
 import UserTabs from "./TabMenu";
-import { info } from "autoprefixer";
+import {
+  ACTIVITY_TIME,
+  FEASIBLE_SCHEDULE,
+  PHYSICAL_ACTIVITY,
+  PHYSICAL_TREATMENT,
+  SELECT_OPTIONS,
+  SPORTS,
+  STRESS_LEVEL,
+  WEEK_ACTIVITY,
+} from "@/constants";
 
 const LifeStyle = () => {
   return (
@@ -54,10 +71,11 @@ const LifeStyle = () => {
                   <span className="text-light-brown">Nivel De Estrés</span>
                 </label>
                 <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg">
-                  <option>Sin Estrés</option>
-                  <option>Ocasional</option>
-                  <option>Repetitivo</option>
-                  <option>Cronico</option>
+                  {STRESS_LEVEL.map((item, index) => (
+                    <option key={index} value={item.value}>
+                      {item.label}
+                    </option>
+                  ))}
                 </select>
               </div>
             </div>
@@ -73,9 +91,11 @@ const LifeStyle = () => {
                   </span>
                 </label>
                 <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg">
-                  <option>Si</option>
-                  <option>No</option>
-                  <option>He Realizado</option>
+                  {PHYSICAL_ACTIVITY.map((item, index) => (
+                    <option key={index} value={item.value}>
+                      {item.label}
+                    </option>
+                  ))}
                 </select>
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
@@ -86,8 +106,11 @@ const LifeStyle = () => {
                   </span>
                 </label>
                 <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg">
-                  <option>Si</option>
-                  <option>No</option>
+                  {PHYSICAL_TREATMENT.map((item, index) => (
+                    <option key={index} value={item.value}>
+                      {item.label}
+                    </option>
+                  ))}
                 </select>
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
@@ -98,23 +121,26 @@ const LifeStyle = () => {
                   </span>
                 </label>
                 <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg">
-                  <option>Ninguno</option>
-                  <option>1-3</option>
-                  <option>3-5</option>
-                  <option>+5</option>
+                  {WEEK_ACTIVITY.map((item, index) => (
+                    <option key={index} value={item.value}>
+                      {item.label}
+                    </option>
+                  ))}
                 </select>
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
                 <label className="flex justify-center text-dark-green">
                   {Info}
-                  <span className="text-light-brown">Actividad Fisica-Tiempo</span>
+                  <span className="text-light-brown">
+                    Actividad Fisica-Tiempo
+                  </span>
                 </label>
                 <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg">
-                  <option>- 15 Min</option>
-                  <option>30 Min</option>
-                  <option>45 Min</option>
-                  <option>60 Min</option>
-                  <option>+ 60 Min</option>
+                  {ACTIVITY_TIME.map((item, index) => (
+                    <option key={index} value={item.value}>
+                      {item.label}
+                    </option>
+                  ))}
                 </select>
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
@@ -123,9 +149,11 @@ const LifeStyle = () => {
                   <span className="text-light-brown">Horario Factible</span>
                 </label>
                 <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg">
-                  <option>Mañana</option>
-                  <option>Tarde</option>
-                  <option>Noche</option>
+                  {FEASIBLE_SCHEDULE.map((item, index) => (
+                    <option key={index} value={item.value}>
+                      {item.label}
+                    </option>
+                  ))}
                 </select>
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
@@ -134,10 +162,11 @@ const LifeStyle = () => {
                   <span className="text-light-brown">Deportes Practicados</span>
                 </label>
                 <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg">
-                  <option>Correr/Trotar</option>
-                  <option>Natación</option>
-                  <option>Entenamiento En Gimansio</option>
-                  <option>Entenamiento En Casa</option>
+                  {SPORTS.map((item, index) => (
+                    <option key={index} value={item.value}>
+                      {item.label}
+                    </option>
+                  ))}
                 </select>
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
@@ -157,118 +186,99 @@ const LifeStyle = () => {
               Hábitos Tóxicos
             </h1>
             <div className="grid grid-cols-1 md:grid-cols-3">
-            <div className="flex justify-center">
-           <div className="flex items-center flex-col m-1 border border-2 border-fluorescent-green rounded-lg m-1 h-50 w-50">
-           <label className="text-dark-green">Consumo De Cigarrillo</label>
-           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-           <div className="m-2 flex flex-col pt-2">
-                <label className="flex justify-center text-dark-green">
-                  {Info}
-                  <span className="text-light-brown">Frecuencia</span>
-                </label>
-                <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48">
-                  <option>No</option>
-                  <option>Ex Fumador</option>
-                  <option>Mensual</option>
-                  <option>2 Veces Por Mes</option>
-                  <option>3 Veces Por Mes</option>
-                  <option>Semanal</option>
-                  <option>2 Veces Por Semana</option>
-                  <option>3 Veces Por Semana</option>
-                  <option>4 Veces Por Semana</option>
-                  <option>5 Veces Por Semana</option>
-                  <option>6 Veces Por Semana</option>
-                  <option>Diario</option>
-                </select>
-              </div>
-              <div className="m-2 flex flex-col pt-2">
-                <label className="flex justify-center text-dark-green">
-                  {Info}
-                  <span className="text-light-brown">Cantidad</span>
-                </label>
-                <input
-                  className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48 "
-                  type="text"
-                />
-              </div>
-              </div>
-           </div>
+              <div className="flex justify-center">
+                <div className="flex items-center flex-col m-1 border border-2 border-fluorescent-green rounded-lg m-1 h-50 w-50">
+                  <label className="text-dark-green">
+                    Consumo De Cigarrillo
+                  </label>
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                    <div className="m-2 flex flex-col pt-2">
+                      <label className="flex justify-center text-dark-green">
+                        {Frequiency}
+                        <span className="text-light-brown">Frecuencia</span>
+                      </label>
+                      <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48">
+                        {SELECT_OPTIONS.map((item, index) => (
+                          <option key={index} value={item.value}>
+                            {item.label}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <div className="m-2 flex flex-col pt-2">
+                      <label className="flex justify-center text-dark-green">
+                        {Info}
+                        <span className="text-light-brown">Cantidad</span>
+                      </label>
+                      <input
+                        className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48 "
+                        type="text"
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className="flex justify-center">
-           <div className="flex items-center flex-col m-1 border border-2 border-fluorescent-green rounded-lg m-1 h-50 w-50">
-           <label className="text-dark-green">Consumo De Alcohol</label>
-           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-           <div className="m-2 flex flex-col pt-2">
-                <label className="flex justify-center text-dark-green">
-                  {Info}
-                  <span className="text-light-brown">Frecuencia</span>
-                </label>
-                <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48">
-                  <option>No</option>
-                  <option>Ex Fumador</option>
-                  <option>Mensual</option>
-                  <option>2 Veces Por Mes</option>
-                  <option>3 Veces Por Mes</option>
-                  <option>Semanal</option>
-                  <option>2 Veces Por Semana</option>
-                  <option>3 Veces Por Semana</option>
-                  <option>4 Veces Por Semana</option>
-                  <option>5 Veces Por Semana</option>
-                  <option>6 Veces Por Semana</option>
-                  <option>Diario</option>
-                </select>
-              </div>
-              <div className="m-2 flex flex-col pt-2">
-                <label className="flex justify-center text-dark-green">
-                  {Info}
-                  <span className="text-light-brown">Cantidad</span>
-                </label>
-                <input
-                  className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48"
-                  type="text"
-                />
-              </div>
-              </div>
-           </div>
+                <div className="flex items-center flex-col m-1 border border-2 border-fluorescent-green rounded-lg m-1 h-50 w-50">
+                  <label className="text-dark-green">Consumo De Alcohol</label>
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                    <div className="m-2 flex flex-col pt-2">
+                      <label className="flex justify-center text-dark-green">
+                        {Frequiency}
+                        <span className="text-light-brown">Frecuencia</span>
+                      </label>
+                      <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48">
+                        {SELECT_OPTIONS.map((item, index) => (
+                          <option key={index} value={item.value}>
+                            {item.label}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <div className="m-2 flex flex-col pt-2">
+                      <label className="flex justify-center text-dark-green">
+                        {Info}
+                        <span className="text-light-brown">Cantidad</span>
+                      </label>
+                      <input
+                        className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48"
+                        type="text"
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className="flex justify-center">
-           <div className="flex items-center flex-col m-1 border border-2 border-fluorescent-green rounded-lg m-1 h-50 w-50">
-           <label className="text-dark-green">Consumo De Drogas</label>
-           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-           <div className="m-2 flex flex-col pt-2">
-                <label className="flex justify-center text-dark-green">
-                  {Info}
-                  <span className="text-light-brown">Frecuencia</span>
-                </label>
-                <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48">
-                  <option>No</option>
-                  <option>Ex Fumador</option>
-                  <option>Mensual</option>
-                  <option>2 Veces Por Mes</option>
-                  <option>3 Veces Por Mes</option>
-                  <option>Semanal</option>
-                  <option>2 Veces Por Semana</option>
-                  <option>3 Veces Por Semana</option>
-                  <option>4 Veces Por Semana</option>
-                  <option>5 Veces Por Semana</option>
-                  <option>6 Veces Por Semana</option>
-                  <option>Diario</option>
-                </select>
+                <div className="flex items-center flex-col m-1 border border-2 border-fluorescent-green rounded-lg m-1 h-50 w-50">
+                  <label className="text-dark-green">Consumo De Drogas</label>
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                    <div className="m-2 flex flex-col pt-2">
+                      <label className="flex justify-center text-dark-green">
+                        {Frequiency}
+                        <span className="text-light-brown">Frecuencia</span>
+                      </label>
+                      <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48">
+                        {SELECT_OPTIONS.map((item, index) => (
+                          <option key={index} value={item.value}>
+                            {item.label}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <div className="m-2 flex flex-col pt-2">
+                      <label className="flex justify-center text-dark-green">
+                        {Info}
+                        <span className="text-light-brown">Cantidad</span>
+                      </label>
+                      <input
+                        className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48"
+                        type="text"
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="m-2 flex flex-col pt-2">
-                <label className="flex justify-center text-dark-green">
-                  {Info}
-                  <span className="text-light-brown">Cantidad</span>
-                </label>
-                <input
-                  className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg w-48"
-                  type="text"
-                />
-              </div>
-              </div>
-              </div>
-           </div>
-              </div>
+            </div>
           </form>
         </div>
       </UserTabs>
