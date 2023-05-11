7d4687e2ef81bfd87c93ce5a41a16f6780c6113b
BernardoTobon <bernardotobon1@gmail.com>
2023-05-11 11:20:49 -0500

    add constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -48,3 +48,123 @@ export const SELECT_OPTIONS = [
     label: "Diario",
   },
 ];
+
+export const STRESS_LEVEL = [
+  {
+    value: "sin estres",
+    label: "Sin Estres",
+  },
+  {
+    value: "ocasional",
+    label: "Ocasional",
+  },
+  {
+    value: "repetitivo",
+    label: "Repetitivo",
+  },
+  {
+    value: "cronico",
+    label: "Cronico",
+  },
+];
+
+export const PHYSICAL_ACTIVITY = [
+  {
+    value: "si",
+    label: "Si",
+  },
+  {
+    value: "no",
+    label: "No",
+  },
+  {
+    value: "he realizado",
+    label: "He Realizado",
+  },
+];
+
+export const PHYSICAL_TREATMENT = [
+  {
+    value: "si",
+    label: "Si",
+  },
+  {
+    value: "no",
+    label: "No",
+  },
+];
+export const WEEK_ACTIVITY = [
+  {
+    value: 'ninguno',
+    label: 'Ninguno',
+  },
+  {
+    value: '1-3',
+    label: '1-3',
+  },
+  {
+    value: '3-5',
+    label: '3-5',
+  },
+  {
+    value: '+5',
+    label: '+5',
+  },
+]
+
+export const ACTIVITY_TIME = [
+  {
+    value: '-15 min',
+    label: '-15 Min',
+  },
+  {
+    value: '30 min',
+    label: '30 Min',
+  },
+  {
+    value: '45 min',
+    label: '45 Min',
+  },
+  {
+    value: '60 min',
+    label: '60 Min',
+  },
+  {
+    value: '+ 60 min',
+    label: '+ 60 Min',
+  },
+]
+
+export const FEASIBLE_SCHEDULE = [
+  {
+    value: 'mañana',
+    label: 'Mañana',
+  },
+  {
+    value: 'tarde',
+    label: 'Tarde',
+  },
+  {
+    value: 'noche',
+    label: 'Noche',
+  },
+]
+
+export const SPORTS = [
+  {
+    value: 'correr/trotar',
+    label: 'Correr/Trotar',
+  },
+  {
+    value: 'natación',
+    label: 'Natación',
+  },
+  {
+    value: 'entrenamiento en gimnasio',
+    label: 'Entrenamiento En Gimnasio',
+  },
+  {
+    value: 'entenamiento en casa',
+    label: 'Entrenamiento En Casa',
+  },
+]
