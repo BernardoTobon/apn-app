553b4045d765c4009ec4a242b612fa28fee16e36
BernardoTobon <bernardotobon1@gmail.com>
2023-05-11 10:26:02 -0500

    Create component

---
+++ src/constants/index.js
@@ -0,0 +1,50 @@
+export const SELECT_OPTIONS = [
+  {
+    value: "no",
+    label: "No",
+  },
+  {
+    value: "ex fumador",
+    label: "Ex Fumador",
+  },
+  {
+    value: "mensual",
+    label: "Mensual",
+  },
+  {
+    value: "2 veces por mes",
+    label: "2 Veces Por Mes",
+  },
+  {
+    value: "3 veces por mes",
+    label: "3 Veces Por Mes",
+  },
+  {
+    value: "semanal",
+    label: "Semanal",
+  },
+  {
+    value: "2 veces por semana",
+    label: "2 Veces Por Semana",
+  },
+  {
+    value: "3 veces por semana",
+    label: "3 Veces Por Semana",
+  },
+  {
+    value: "4 veces por semana",
+    label: "4 Veces Por Semana",
+  },
+  {
+    value: "5 veces por semana",
+    label: "5 Veces Por Semana",
+  },
+  {
+    value: "6 veces por semana",
+    label: "6 Veces Por Semana",
+  },
+  {
+    value: "diario",
+    label: "Diario",
+  },
+];
