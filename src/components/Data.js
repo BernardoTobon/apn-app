739e8da99b974d2d1a5572d9144585c610be667a
BernardoTobon <bernardotobon1@gmail.com>
2023-05-12 12:00:43 -0500

    update code

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,5 +1,6 @@
 import { Address, Age, Birthday, CellPhone, City, Department, Di, Education, Email, Gender, Info, Phone } from "@/icons";
 import UserTabs from "./TabMenu";
+import { GENDER } from "@/constants";
 
 const UserData = () => {
   return (
@@ -82,8 +83,11 @@ const UserData = () => {
                   <span className="text-light-brown">Genero</span>
                 </label>
                 <select className=" text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg">
-                  <option>Hombre</option>
-                  <option>Mujer</option>
+                {GENDER.map((item, index) => (
+                    <option key={index} value={item.value}>
+                      {item.label}
+                    </option>
+                  ))}
                 </select>
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
