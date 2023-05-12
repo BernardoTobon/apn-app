00f59202dc811265837c238bd4c2b979534cb0c0
BernardoTobon <bernardotobon1@gmail.com>
2023-05-12 12:00:12 -0500

    update code

--- src/components/UserHistory.js
+++ src/components/UserHistory.js
@@ -37,11 +37,11 @@ const UserHistory = () => {
                   <span className="text-light-brown">Tipo De Sangre</span>
                 </label>
                 <select className="text-center text-light-brown border border-2 border-dark-green focus:outline-none m-1 rounded-lg">
-                 {
-                  BLOOD_TYPE.map (
-                    (item, index) => <option key={index} value={item.value}>{item.label}</option>
-                  )
-                 }
+                  {BLOOD_TYPE.map((item, index) => (
+                    <option key={index} value={item.value}>
+                      {item.label}
+                    </option>
+                  ))}
                 </select>
               </div>
               <div className="m-2 flex flex-col border-2 border-fluorescent-green rounded-lg h-24 w-72 pt-8">
