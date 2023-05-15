d8b8dc2566bebd022095df9c7331059b03c24f43
BernardoTobon <bernardotobon1@gmail.com>
2023-05-15 12:27:17 -0500

    add router and style

--- src/components/TabMenu.js
+++ src/components/TabMenu.js
@@ -5,44 +5,44 @@ const UserTabs = ({ children }) => {
   return (
     <>
       <div className="flex justify-center pt-2 ">
-        <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
-          <li class="mr-2">
+        <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
+          <li className="mr-2">
             <a
               href="/user/data"
               aria-current="page"
-              class={`${router.pathname == "/user/data"?"bg-gray-100":""} inline-block p-4 text-dark-green rounded-t-lg hover:bg-gray-50 active`}
+              className={`${router.pathname == "/user/data"?"bg-gray-100":""} inline-block p-4 text-dark-green rounded-t-lg hover:bg-gray-50 active`}
             >
               Datos
             </a>
           </li>
-          <li class="mr-2">
+          <li className="mr-2">
             <a
               href="/user/userhistory"
-              class={`${router.pathname == "/user/userhistory"?"bg-gray-100":""} inline-block p-4 text-dark-green rounded-t-lg hover:bg-gray-50 active `}
+              className={`${router.pathname == "/user/userhistory"?"bg-gray-100":""} inline-block p-4 text-dark-green rounded-t-lg hover:bg-gray-50 active `}
             >
               Historia clinica
             </a>
           </li>
-          <li class="mr-2">
+          <li className="mr-2">
             <a
               href="/user/lifestyle"
-              class={`${router.pathname == "/user/lifestyle"?"bg-gray-100":""} inline-block p-4 text-dark-green rounded-t-lg hover:bg-gray-50 active`}
+              className={`${router.pathname == "/user/lifestyle"?"bg-gray-100":""} inline-block p-4 text-dark-green rounded-t-lg hover:bg-gray-50 active`}
             >
               Estilo de vida
             </a>
           </li>
-          <li class="mr-2">
+          <li className="mr-2">
             <a
               href="/user/goals"
-              class={`${router.pathname == "/user/goals"?"bg-gray-100":""} inline-block p-4 text-dark-green rounded-t-lg hover:bg-gray-50 active`}
+              className={`${router.pathname == "/user/goals"?"bg-gray-100":""} inline-block p-4 text-dark-green rounded-t-lg hover:bg-gray-50 active`}
             >
               Objetivos
             </a>
           </li>
-          <li class="mr-2">
+          <li className="mr-2">
             <a
               href="#"
-              class="inline-block p-4 rounded-t-lg text-dark-green hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+              className="inline-block p-4 rounded-t-lg text-dark-green hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
             >
               Recomendaciones
             </a>
