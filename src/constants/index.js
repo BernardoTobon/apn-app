ab26cbd191b05e27d0c7ee9a20b4858eb5ffb589
BernardoTobon <bernardotobon1@gmail.com>
2023-05-16 17:22:40 -0500

    add const

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,50 @@
+export const GENDER = [
+  {
+    value: 'hombre',
+    label: 'Hombre',
+  },
+  {
+    value: 'mujer',
+    label: 'Mujer',
+  },
+
+]
+
+export const BLOOD_TYPE = [
+  {
+    value: 'A+',
+    label: 'A+',
+  },
+  {
+    value: 'A-',
+    label: 'A-',
+  },
+  {
+    value: 'B+',
+    label: 'B+',
+  },
+  {
+    value: 'B-',
+    label: 'B-',
+  },
+  {
+    value: 'O+',
+    label: 'O+',
+  },
+  {
+    value: 'O+',
+    label: 'O-',
+  },
+  {
+    value: 'AB+',
+    label: 'AB+',
+  },
+  {
+    value: 'AB-',
+    label: 'AB-',
+  },
+]
+
 export const SELECT_OPTIONS = [
   {
     value: "no",
