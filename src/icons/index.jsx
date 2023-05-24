0fbd2bada74704c4d898a38f87858c10060d03ef
BernardoTobon <bernardotobon1@gmail.com>
2023-05-24 11:50:50 -0500

    add styles

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -5,7 +5,7 @@ export const AddIcon = (
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="w-6 h-6"
+    className="w-6 h-6 text-dark-green m-2"
   >
     <path
       strokeLinecap="round"
