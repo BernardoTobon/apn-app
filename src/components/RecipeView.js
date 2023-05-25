8bbf8cae429a107b767b45bcdd20c8f9103583f6
BernardoTobon <bernardotobon1@gmail.com>
2023-05-25 16:50:12 -0500

    create component

---
+++ src/components/RecipeView.js
@@ -0,0 +1,16 @@
+const RecipeView = () => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <label className="border border-2 border-dark-green border-t-0 border-r-0 border-l-0 rounded-lg m-4">
+          Nombre De La Receta
+        </label>
+      </div>
+      <div className="flex justify-center">
+        <div className="border boder-2 border-fluorescent-green h-64 w-64 rounded-lg"></div>
+      </div>
+    </>
+  );
+};
+
+export default RecipeView;
