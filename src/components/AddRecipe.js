fe65f89ff5b8e12f41218529124a6645997f45fc
BernardoTobon <bernardotobon1@gmail.com>
2023-05-24 19:34:36 -0500

    add form and styles

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,7 +1,35 @@
-const AddRecipe = () =>{
-    return <>
-    
-    </>
-}
 
-export default AddRecipe
+const AddRecipe = () => {
+  return (
+    <div className="flex justify-center">
+      <form>
+        <div >
+        <div className="flex justify-center">
+        <input
+          type="text"
+          placeholder="Nombre De La Receta"
+          className="border border-2 border-dark-green rounded-lg m-8 focus:outline-none text-dark-green text-center"
+        />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3">
+        <div className="flex flex-col items-center m-2">
+            <label className="text-dark-green">Imagen</label>
+          <input type="file" 
+          className="border border-2 border-fluorescent-green focus:outline-none"/>
+        </div>
+        <div className="flex flex-col items-center m-2">
+            <label className="text-dark-green">Ingredientes</label>
+          <textarea className="border border-2 border-fluorescent-green focus:outline-none"/>
+        </div>
+        <div className="flex flex-col items-center m-2">
+            <label className="text-dark-green">Preparacion</label>
+          <textarea className="border border-2 border-fluorescent-green focus:outline-none"/>
+        </div>
+        </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddRecipe;
