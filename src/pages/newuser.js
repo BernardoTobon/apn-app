7bed86b7a5643e98833fec334000b422df0527d5
BernardoTobon <bernardotobon1@gmail.com>
2023-05-25 18:08:35 -0500

    create route

---
+++ src/pages/newuser.js
@@ -0,0 +1,11 @@
+const { default: NewUser } = require("@/components/NewUser");
+
+const NewUserPage = () => {
+  return (
+    <>
+      <NewUser />
+    </>
+  );
+};
+
+export default NewUserPage;
