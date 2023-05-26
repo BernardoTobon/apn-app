546300dc9f83fb2227dc953fe7da1e2dff1087f3
BernardoTobon <bernardotobon1@gmail.com>
2023-05-25 19:17:18 -0500

    add components

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,10 +1,41 @@
-const NewUser = () => {
-    return<>
-    <div className="flex justify-center">
-        <h1></h1>
+import Accordion from "./Accordion";
+import Data from "./Data";
+import Goals from "./Goals";
+import LifeStyle from "./LifeStyle";
+import UserHistory from "./UserHistory";
 
-    </div>
+const NewUser = () => {
+  return (
+    <>
+      <h1 className="m-3 flex justify-center font-extrabold  text-transparent text-2xl bg-clip-text bg-gradient-to-r from-light-brown to-dark-green">
+        Nuevo Usuario
+      </h1>
+      <div className="flex justify-center">
+        <div className="flex flex-col">
+            <div className="m-2">
+            <Accordion>
+            <Data />
+          </Accordion>
+            </div>
+         <div className="m-2">
+         <Accordion>
+            <UserHistory/>
+          </Accordion>
+         </div>
+         <div className="m-2">
+         <Accordion>
+            <LifeStyle/>
+          </Accordion>
+         </div>
+         <div className="m-2">
+         <Accordion>
+            <Goals/>
+          </Accordion>
+         </div>
+        </div>
+      </div>
     </>
-}
+  );
+};
 
 export default NewUser;
