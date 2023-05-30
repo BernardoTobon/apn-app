81e3d83f92c15a2bf4cee49a2834ea617da5efe5
BernardoTobon <bernardotobon1@gmail.com>
2023-05-30 11:40:09 -0500

    create component

---
+++ src/components/Measurements.js
@@ -0,0 +1,31 @@
+import { Line } from "react-chartjs-2"
+import {
+    Chart as chartjs,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    ToolTip,
+    Legend,
+    Filler,
+} from 'chart.js';
+
+chartjs.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    ToolTip,
+    Legend,
+    Filler
+)
+
+const Measurements = () => {
+    return <>
+    
+    </>
+}
+
+export default Measurements
