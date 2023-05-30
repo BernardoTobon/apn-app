a4a49d0a7fc38a893a9948f4ceaf3a6ecaa1c48b
BernardoTobon <bernardotobon1@gmail.com>
2023-05-30 12:28:52 -0500

    update code

--- src/components/Measurements.js
+++ src/components/Measurements.js
@@ -1,31 +1,5 @@
-import { Line } from "react-chartjs-2"
-import {
-    Chart as chartjs,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    ToolTip,
-    Legend,
-    Filler,
-} from 'chart.js';
-
-chartjs.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    ToolTip,
-    Legend,
-    Filler
-)
-
 const Measurements = () => {
     return <>
     
     </>
 }
-
-export default Measurements
