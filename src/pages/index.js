acc1daf25569596217e3f1d57fc503966f093a78
BernardoTobon <bernardotobon1@gmail.com>
2023-06-21 11:50:00 -0500

    import for test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,16 @@
+import RecipeForm from "@/components/recipe/AddRecipe"
+import AddRecipe from "@/components/recipe/AddRecipe"
 import StartHome from "@/components/Home"
+import Measurements from "@/components/Measurements"
+import Recipe from "@/components/recipe/Recipe"
+import RecipeView from "@/components/recipe/RecipeView"
+import Folds from "@/components/data/Folds"
+import Perimeters from "@/components/data/Perimeters"
+import Diameters from "@/components/data/DIameters"
+import NewUser from "@/components/NewUser"
+import ImcGraphics from "@/components/data/ImcGraphics"
 export default function Home() {
-  return ( <StartHome/>
+  return ( <ImcGraphics/>
           
   )
 }
